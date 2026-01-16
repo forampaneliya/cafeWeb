141c40cbf7bf1c43787d9e6b5e94ea5737ae228a
vidhisavaliya1710 <vidhisavaliya017@gmail.com>
2026-01-16 16:09:11 +0530

    solve error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 
 import { useState } from 'react'
 import './App.css'
-import AllRoutes from './AllRoutes'
-import Header from './layout/Header'
+// import Header from './layout/Header'
 import Footer from './layout/Footer'
+import AllRoute from './AllRoute'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <AllRoutes />
+      {/* <Header /> */}
+      <AllRoute/>
       <Footer />
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
-import App from './App'
+// import App from './App'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
